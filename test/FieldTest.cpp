284f1eb94e7d4fa318a6e19fd918817ff216a289
arendellian13 <listener1381@outlook.com>
2025-05-23 13:10:20 +0800

    test: Add test for LambdaField

---
+++ test/FieldTest.cpp
@@ -0,0 +1,53 @@
+//
+// Created by Renatus Madrigal on 5/23/2025.
+//
+
+#include "phosphorus/Field.h"
+#include "phosphorus/Vector.h"
+#include <gtest/gtest.h>
+
+TEST(FieldTest, LambdaField) {
+  using namespace phosphorus;
+
+  // Define a simple coordinate type
+  using SimpleCoordinate = Cartesian3D;
+
+  // Define a simple particle type
+  struct SimpleParticle : public CommonParticle {};
+
+  // Create a lambda function for the force
+  auto force_function = [](const SimpleCoordinate &coord,
+                           const SimpleParticle &particle) {
+    return coord * particle.mass();
+  };
+
+  // Create a LambdaField instance
+  LambdaField field(force_function);
+
+  // Create a coordinate and a particle
+  SimpleCoordinate coord{};
+  coord = {1.0, 2.0, 3.0};
+  SimpleParticle particle;
+  particle.mass() = 5.0;
+
+  // Calculate the force
+  auto force = field.force(coord, particle);
+
+  // Check the result
+  auto expected_force = coord * particle.mass();
+  EXPECT_EQ(force, expected_force);
+
+  auto force_coefficient = 2.718;
+
+  auto force_function_with_coefficient =
+      [force_coefficient](const SimpleCoordinate &coord,
+                          const SimpleParticle &particle) {
+        return force_coefficient * coord * particle.mass();
+      };
+
+  LambdaField field_with_coefficient(force_function_with_coefficient);
+
+  auto force2 = field_with_coefficient.force(coord, particle);
+  auto expected_force2 = force_coefficient * coord * particle.mass();
+  EXPECT_EQ(force2, expected_force2);
+}
