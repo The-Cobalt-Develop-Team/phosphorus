618340ff371af430959277af9c7adb6b684c3f2f
arendellian13 <listener1381@outlook.com>
2025-05-24 15:37:27 +0800

    build: Add function to simplify example executable creation in CMake

--- cmake/CMakeUtilities.cmake
+++ cmake/CMakeUtilities.cmake
@@ -3,3 +3,13 @@ function(phosphorus_option opt_name opt_desc opt_default)
     option(${opt_name} "${opt_desc}" ${opt_default})
     message(STATUS "${opt_name}: [${${opt_name}}]")
 endfunction()
+
+function(phosphorus_add_example example_name)
+    set(example_source "${PHOSPHORUS_EXAMPLES_DIR}/${example_name}.cpp")
+    set(example_target "${example_name}")
+
+    add_executable(${example_target} ${example_source})
+    target_link_libraries(${example_target} PRIVATE phosphorus)
+    set_target_properties(${example_target} PROPERTIES OUTPUT_NAME ${example_name})
+    message(STATUS "Added example: ${example_name}")
+endfunction()
