6072773b05bfb4991f382a29fb253e471caf836f
arendellian13 <listener1381@outlook.com>
2025-05-24 10:27:19 +0800

    test: Update FieldTest to use evaluate method and add ModifiableField class

--- test/FieldTest.cpp
+++ test/FieldTest.cpp
@@ -5,6 +5,7 @@
 #include "phosphorus/Field.h"
 #include "phosphorus/Vector.h"
 #include <gtest/gtest.h>
+#include <iostream>
 
 using namespace phosphorus;
 
@@ -31,10 +32,10 @@ TEST(FieldTest, LambdaField) {
   particle.mass() = 5.0;
 
   // Calculate the force
-  auto force = field.force(coord, particle);
+  auto force = field.evaluate(coord, particle);
 
   // Check the result
-  auto expected_force = coord.toCartesian() * particle.mass();
+  auto expected_force = Vector<3>{1.0, 2.0, 3.0} * particle.mass();
   EXPECT_EQ(force, expected_force);
 
   auto force_coefficient = 2.718;
@@ -47,13 +48,13 @@ TEST(FieldTest, LambdaField) {
 
   LambdaField field_with_coefficient(force_function_with_coefficient);
 
-  auto force2 = field_with_coefficient.force(coord, particle);
+  auto force2 = field_with_coefficient.evaluate(coord, particle);
   auto expected_force2 =
       force_coefficient * coord.toCartesian() * particle.mass();
   EXPECT_EQ(force2, expected_force2);
 }
 
-TEST(FieldTest, FieldAdd) {
+TEST(FieldTest, FieldOperations) {
   auto force_function1 = [](const Cartesian3D &point,
                             const CommonParticle &particle) {
     return point.toCartesian() * particle.mass();
@@ -69,13 +70,60 @@ TEST(FieldTest, FieldAdd) {
 
   CommonParticle particle{1.0, 2.0};
   Cartesian3D position{3.0, 4.0, 5.0};
-  auto composite_force = composite_field.force(position, particle);
+  auto composite_force = composite_field.evaluate(position, particle);
   auto expected_force =
-      field1.force(position, particle) + field2.force(position, particle);
+      field1.evaluate(position, particle) + field2.evaluate(position, particle);
   EXPECT_EQ(composite_force, expected_force);
 
   auto negative_field = -field1;
-  composite_force = negative_field.force(position, particle);
-  expected_force = -field1.force(position, particle);
+  composite_force = negative_field.evaluate(position, particle);
+  expected_force = -field1.evaluate(position, particle);
   EXPECT_EQ(composite_force, expected_force);
+}
+
+TEST(FieldTest, FieldOperationWithModification) {
+  auto force_function = [](const Cartesian3D &point,
+                           const CommonParticle &particle) {
+    return point.toCartesian() * particle.mass();
+  };
+  auto field = LambdaField(force_function);
+
+  struct ModifiableField : public BaseField<ModifiableField, Cartesian3D> {
+    using CoordinateVecType = Cartesian3D;
+    using Vector = typename CoordinateVecType::Vector;
+
+    explicit ModifiableField(double scalar) : scalar_(scalar) {}
+
+    void setScalar(double scalar) { scalar_ = scalar; }
+
+    [[nodiscard]] Vector evaluate(const CoordinateVecType &coord,
+                                  const CommonParticle &particle) const {
+      return coord.toCartesian() * particle.mass() * scalar_;
+    }
+
+  private:
+    double scalar_ = 0;
+  };
+
+  auto modifiable_field = ModifiableField(2.0);
+  auto composite_field = field + modifiable_field;
+
+  CommonParticle particle{1.0, 2.0};
+  Cartesian3D position{3.0, 4.0, 5.0};
+  auto modified_force = composite_field.evaluate(position, particle);
+  auto expected_force = field.evaluate(position, particle) +
+                        modifiable_field.evaluate(position, particle);
+  EXPECT_EQ(modified_force, expected_force);
+  std::cout << "Modified force: " << modified_force << std::endl;
+  std::cout << "Expected force: " << expected_force << std::endl;
+
+  modifiable_field.setScalar(4.0);
+  auto modified_force2 = composite_field.evaluate(position, particle);
+  auto expected_force2 = field.evaluate(position, particle) +
+                         modifiable_field.evaluate(position, particle);
+  EXPECT_EQ(modified_force2, expected_force2);
+  EXPECT_NE(modified_force, modified_force2);
+
+  std::cout << "Modified force2: " << modified_force2 << std::endl;
+  std::cout << "Expected force2: " << expected_force2 << std::endl;
 }
